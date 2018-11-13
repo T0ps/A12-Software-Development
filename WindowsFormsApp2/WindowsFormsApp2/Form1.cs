6d0cf822a9e59ce2f2ff0b699b1f6d47c96f1b31
Matt <mjtopham@icloud.com>
2018-11-13 17:53:21 +0000

    Minor changes to Dropdown

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -13,49 +13,51 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+
+        public string searchFunction;
+        public string searchVal;
+
         public Form1()
         {
             InitializeComponent();
-            movieCombo.Items.Add("TMDb");
+
             movieCombo.Items.Add("OMDb");
+            movieCombo.Items.Add("TMDb");
+            movieCombo.SelectedIndex = 0;
+
+ 
+
+            if (movieCombo.SelectedText == "OMDb")
+            {
+                MessageBox.Show("OMDb");
+
+                searchFunction = "http://www.omdbapi.com/?apikey=d111ba84&" + "t=" + searchVal + "&r=xml";
+                
+            }
+
+            else if (movieCombo.SelectedText == "TMDb")
+            {
+                MessageBox.Show("TMDb");
+
+                searchFunction = "https://api.themoviedb.org/3/movie/550?api_key=95933e5a60f119e9b19b8e2be7252893" + "/search" + searchVal;
+            }
+
+
         }
 
         private void searchBar_TextChanged(object sender, EventArgs e)
         {
-
+            searchVal = searchBar.Text;
         }
 
         private void searchButton_Click(object sender, EventArgs e)
         {
-            string searchVal;
-            searchVal = searchBar.Text;
 
-            string searchFunction;
+            System.Diagnostics.Process.Start(searchFunction);
 
-            Console.WriteLine(searchVal);
-
-            if (movieCombo.SelectedText == "TMDb")
-            {
-                searchFunction = "http://www.omdbapi.com/?apikey=d111ba84&" + "t=" + searchVal + "&r=xml";
-                System.Diagnostics.Process.Start(searchFunction);
-            }   
-
-            else if (movieCombo.SelectedText == "OMDb")
-            {
-                searchFunction = "https://api.themoviedb.org/3/movie/550?api_key=95933e5a60f119e9b19b8e2be7252893" + "/search" + searchVal;
-                System.Diagnostics.Process.Start(searchFunction);
-            }
-
-
-
-            
             //string uri = searchFunction;
             //XDocument document = XDocument.Load(uri);
             //richTextBox1.Text = Convert.ToString(document);
-
-
-
-
         }
     }
 }
