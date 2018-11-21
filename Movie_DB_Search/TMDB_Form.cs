2602f341741dc6918204b7820fdeb657e2ccf47a
T0ps <mjtopham@icloud.com>
2018-11-21 17:38:34 +0000

    New project with separate forms

---
+++ Movie_DB_Search/TMDB_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movie_DB_Search
+{
+    public partial class TMDB_Form : Form
+    {
+        public TMDB_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
